7fcfd9411aa3524a0629e80aa18b97005a11f5c6
VILAIDED <65062161+VILAIDED@users.noreply.github.com>
2021-11-19 14:24:59 +0700

    update chuc nang

--- PBL3/GUI/FrmCon/FrmCTBan.cs
+++ PBL3/GUI/FrmCon/FrmCTBan.cs
@@ -9,27 +9,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 namespace PBL3.GUI.FrmCon
 {
-    public partial class FrmCTNhap : Form
+    public partial class FrmCTBan : Form
     {
-        string maPhieuNhap = "";
-        public FrmCTNhap(string maPN)
+        string maPhieuXuat;
+        public FrmCTBan(String ma)
         {
             InitializeComponent();
-            maPhieuNhap = maPN;
+            maPhieuXuat = ma;
         }
 
-        private void FrmCTNhap_Load(object sender, EventArgs e)
+        private void FormCTBan_Load(object sender, EventArgs e)
         {
-
-            foreach (CTPhieuNhap ctNhap in BLL_ThongKe.Instance.getCTPhieuNhapById_BLL(maPhieuNhap))
+            foreach (CTPhieuXuat ctXuat in BLL_ThongKe.Instance.getCTPhieuXuatById_BLL(maPhieuXuat))
             {
                 ListViewItem listViewItem = new ListViewItem();
-                listViewItem.Text = BLL_ThongKe.Instance.getTenSanPhamByID_BLL(ctNhap.maSP);
-                listViewItem.SubItems.Add(ctNhap.soLuong.ToString());   
+                listViewItem.Text = BLL_ThongKe.Instance.getTenSanPhamByID_BLL(ctXuat.maSp);
+                listViewItem.SubItems.Add(ctXuat.soLuong.ToString());
+                listViewItem.SubItems.Add(ctXuat.thanhTien.ToString());
                 listView1.Items.Add(listViewItem);
+
             }
+
         }
 
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
